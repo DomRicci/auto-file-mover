2412294917e18f7912c930ee62e8277c18374798
Dom Ricci <dom.ricci@outlook.com>
2020-05-06 16:39:46 +0100

    Outputs progress and success messages to a text file

--- auto_file_mover.py
+++ auto_file_mover.py
@@ -7,11 +7,11 @@ import time
 class myHandler(FileSystemEventHandler):
 
     def on_modified(self, event):
-        print("event triggered")
+
         for filename in os.listdir(folder_to_track):
-            print("moving file" + filename)
+            doc = open("actions.txt", "a")
+            doc.write("INPROGRESS moving " + filename )
             file_extension = os.path.splitext(filename)[1]
-            print(file_extension)
             src = folder_to_track + "\\" + filename     # Path for file to be moved
 
             if file_extension == ".pdf":    # Locates destination folder
@@ -24,6 +24,8 @@ class myHandler(FileSystemEventHandler):
                 folder_destination = "C:\\DATA\\MISC_downloads\\"
             new_destination = folder_destination + filename     # Path to be moved too
             os.rename(src, new_destination)
+            doc.write(" SUCCESS moved " + filename + " to " + folder_destination + "\n")
+            doc.close()
 
 
 folder_to_track = "C:\\users\\DomRi\\Downloads"
@@ -33,13 +35,9 @@ event_handler = myHandler()  # initialises myHandler
 my_observer = Observer()    # initialises observer (imported from watchdog)
 my_observer.schedule(event_handler, folder_to_track, recursive=True)
 my_observer.start()
-i = 1
 try:
     while True:
-        time.sleep(10)
-        i = i + 1
-        print("check" + folder_to_track)
-        print(i)
+        time.sleep(10)        
 except KeyboardInterrupt:
     my_observer.stop()
 my_observer.join()
